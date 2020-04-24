003c86718147323a9a7e0db8721f858df71fcc9d
JOE-JOE-NGIGI <jngigiw@gmail.com>
2020-04-24 14:17:49 +0300

    fetch from camera

---
+++ getting_started_with_images.py
@@ -0,0 +1,15 @@
+import cv2
+
+# to read from file, you can  give the file name in place of 0
+cap = cv2.VideoCapture(0); #or -1 1 for second cam
+
+while (True):
+    ret, frame = cap.read() #retirn true if frame is available
+    #change to gray scale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
