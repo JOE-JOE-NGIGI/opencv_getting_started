3959be0d257f9c3a042670cf32e6a949390c7ee0
JOE-JOE-NGIGI <jngigiw@gmail.com>
2020-04-26 17:52:32 +0300

    Add line and rectangle

--- getting_started_with_images.py
+++ getting_started_with_images.py
@@ -4,12 +4,15 @@ import cv2
 cap = cv2.VideoCapture(0); #or -1 1 for second cam
 
 while (True):
-    ret, frame = cap.read() #retirn true if frame is available
+    ret, frame = cap.read() #return true if frame is available
+
     #change to gray scale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
 
+    #Use keyboard character 'q' to terminate
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
